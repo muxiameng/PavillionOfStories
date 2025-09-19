0bf3b78ab910d2334ea041f945aa915dd178bcf7
muxiameng <muxiameng86@gmail.com>
2025-09-19 10:54:00 +0900

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menuToggle');
+const sidebar = document.querySelector('.sidebar');
+
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+});
