b88f13aefd36799c05d69c940addd15cce6d99cc
muxiameng <muxiameng86@gmail.com>
2025-09-19 16:25:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 const menuToggle = document.getElementById('menuToggle');
-const sidebar = document.querySelector('.sidebar');
+const sidebar = document.getElementById('sidebar');
+const sidebarLinks = document.querySelectorAll('.sidebar-link');
 
 menuToggle.addEventListener('click', () => {
-  sidebar.classList.toggle('open');
+  sidebar.classList.toggle('closed');
+});
+
+// SPのとき、サイドバーリンク押下でメニューを閉じる
+sidebarLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    if (window.innerWidth <= 768) {
+      sidebar.classList.add('closed');
+    }
+  });
+});
+
+// リサイズ時にハンバーガーメニュー表示切り替え
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    sidebar.classList.remove('closed'); // PCは常に開く
+  }
 });
